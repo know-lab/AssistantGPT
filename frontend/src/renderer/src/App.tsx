ff4a866b5bb9162e6f71bfa144d4ac22674c4840
AlexMilos <11dmilosa@gmail.com>
2023-10-09 20:56:22 +0200

    feat: changed to context fom state in home

--- frontend/src/renderer/src/App.tsx
+++ frontend/src/renderer/src/App.tsx
@@ -1,13 +1,19 @@
+import { useContext } from 'react'
 import Chat from './components/Chat'
+import WorkflowList from './components/WorkflowList'
 import './styles/main.scss'
+import { ActiveTabContext } from './components/ContextProvider'
 
 function App(): JSX.Element {
+  const [activeTab, setActiveTab] = useContext(ActiveTabContext)
   return (
     <>
       <div className="header" />
       <main className="home">
-        <div className="home__side"></div>
-        <Chat />
+        <div className="home__side">
+          <WorkflowList />
+        </div>
+        {activeTab === 'chat' && <Chat />}
       </main>
     </>
   )
