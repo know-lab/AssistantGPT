77a565f0b2af06c7be3483ab85a9bcdff021e6ce
AlexMilos <11dmilosa@gmail.com>
2023-10-30 22:44:06 +0100

    feat: added profile badge to the home screen

--- frontend/src/renderer/src/App.tsx
+++ frontend/src/renderer/src/App.tsx
@@ -3,6 +3,7 @@ import WorkflowList from './components/WorkflowList'
 import './styles/main.scss'
 import { useActiveTab } from './components/ContextProvider'
 import WorkflowDetails from './components/WorkflowDetails'
+import ProfileBadge from './components/ProfileBadge'
 
 function App(): JSX.Element {
   const [activeTab, setActiveTab] = useActiveTab()
@@ -11,7 +12,13 @@ function App(): JSX.Element {
       <div className="header" />
       <main className="home">
         <div className="home__side">
-          <WorkflowList />
+          <div className="home__side__profile">
+            <ProfileBadge />
+          </div>
+          <div className="home__side__separator" />
+          <div className="home__side__workflow-list ">
+            <WorkflowList />
+          </div>
         </div>
         {activeTab === 'chat' && <Chat />}
         {activeTab === 'workflow' && <WorkflowDetails />}
